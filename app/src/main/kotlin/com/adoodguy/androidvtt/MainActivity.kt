01a0a52469b86f1c7054a10feb7f7a0d67289263
ADoodGuy <njiorbe@gmail.com>
2026-08-08 17:47:04 +0800

    Initialize and host tabletop map management

--- app/src/main/kotlin/com/adoodguy/androidvtt/MainActivity.kt
+++ app/src/main/kotlin/com/adoodguy/androidvtt/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import com.adoodguy.androidvtt.tabletop.TabletopMapHost
+import com.adoodguy.androidvtt.tabletop.TabletopMapStore
 import com.adoodguy.androidvtt.tabletop.TabletopScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TabletopMapStore.initialize(applicationContext)
         setContent {
             MaterialTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    TabletopScreen()
+                    TabletopMapHost {
+                        TabletopScreen()
+                    }
                 }
             }
         }
